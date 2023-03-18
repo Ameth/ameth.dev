c0a1e189aaefe3d01edb8c1c07a245d574161351
Ameth Ordoñez <amethgabriel@hotmail.com>
2023-03-18 12:38:10 -0500

    Add Tailwind and config Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,22 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import tailwind from '@astrojs/tailwind'
+
+// https://astro.build/config
+export default defineConfig({
+  markdown: {
+    shikiConfig: {
+      // Escoge entre los temas integrados de Shiki (o agrega los tuyos propios)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: 'dracula',
+      // Agrega lenguajes de programación personalizados
+      // Nota: Shiki tiene innumerables lenguajes de programación incorporados, ¡incluido .astro!
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      langs: [],
+      // Habilita word wrap para evitar el desplazamiento horizontal
+      wrap: true,
+    },
+  },
+  integrations: [tailwind()]
+});
---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+
+// const remark = require('remark')
+
+// import { remark } from 'remark'
+
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+  },
+  // transform: {
+  //   md: (content) => {
+  //     return remark().process(content)
+  //   },
+  // },
+  plugins: [],
+}
